f36a445895ee75ea5135c5570694b20b42ab4adf
Home <dsshvakin@mail.ru>
2024-02-04 19:08:16 +0300

    Дз_№2

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-def stop_story():
-    global step
-    step = 0
-
-
-while step > 0:
-    if step == 1:
-        print("Вы пришли к развилке ")
-        print("куда идти")
-        print("направо 0")
-        print("налево 1")
-        answer = int(input())
-        if answer == 0:
-            print("Вы пошли направо")
-            step = 2
-        if answer == 1:
-            print("вы пошли налево")
-            step = 3
-    if step == 2:
-        print("Вы пришли к развилке")
-        print("Куда идти")
-        print("направо 0")
-        print("налево 1")
-        answer = int(input())
-        if answer == 0:
-            print("Вы пошли направо")
-            print("Вы умерли")
-            raise SystemExit
-        if answer == 1:
-            print("вы пошли налево")
-            step = 3
+#План решения задания №2
+"""
+1) Перевести буквы в цифры
+2) Сложить буквенные значения
+3) Буквы перевести в 4 систему исчесления
+4) И по делить между собой
+"""
