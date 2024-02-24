48fcfccaa05fee62eb8029a72b5bef5de4a532e7
Home <dsshvakin@mail.ru>
2024-02-24 15:14:28 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
-class App(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi("from.ui", self)
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ex = App()
-    ex.show()
-    sys.exit(app.exec())
